bb5c2a925318c1184f08bffdb4dcd862df526c2f
Sajan Thiara <sajan4@hotmail.com>
2021-04-29 00:34:57 +0000

    exercises 5, 6, 7 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.update(name: "Nanaimo")
+@store1.employees.create(first_name: "Josh", last_name: "Reuben", hourly_rate: 40)
+@store1.employees.create(first_name: "Andrew", last_name: "Baker", hourly_rate: 60)
+@store2.employees.create(first_name: "Kyle", last_name: "Rizzo", hourly_rate: 100)
+@store2.employees.create(first_name: "George", last_name: "Costanza", hourly_rate: 80)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :at_least_one_clothing_type
 
+  def at_least_one_clothing_type
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must carry one of men's or women's apparel")
+      errors.add(:womens_apparel, "must carry one of men's or women's apparel")
+    end
+  end
 end
